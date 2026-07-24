c80c61855d0a264e9a75bd901ee12c2e8cb7b031
ikerchaid <iker.acosta.compu@gmail.com>
2026-07-24 16:36:56 -0700

    Add files via upload

---
+++ suma.py
@@ -0,0 +1,24 @@
+
+n1=int
+n2=int
+suma=int
+resta=int
+mult=int
+div=int
+
+
+
+n1= int(input("pick first number:"))
+n2= int(input("pick second number:"))
+suma=n1 + n2
+print("suma:",suma)
+print()
+resta=n1 - n2
+print("resta:", resta)
+print()
+mult=n1 * n2
+print("multiplicacion:",mult)
+print()
+div= n1 / n2
+print("division:",div)
+print()
